da8c3580679eef7e5f90e6be574aa6d37423cc42
er-ashu-sharma <112608038+er-ashu-sharma@users.noreply.github.com>
2024-07-20 13:31:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+    return "Hello From Azure!"
 if __name__ == "__main__":
     app.run()
