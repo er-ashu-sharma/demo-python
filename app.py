3531b496bc4852f59ff98e3ed4a89bcdf98d3d46
ashu sharma <ashusharma10071999@gmail.com>
2024-07-26 12:24:48 -0400

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+
+    students = [
+        {"id": "c0894214", "name": "ashu sharma"},
+        {"id": "c0894124", "name": "sarpreet"},
+        {"id": "c0897852", "name": "harkirat singh"},
+        {"id": "c0893576", "name": "jasleen kaur chopra"}
+    ]
+    
+    html_content = "<h1>Student IDs and Names</h1>"
+    html_content += "<ul>"
+    for student in students:
+        html_content += f"<li>{student['id']}: {student['name']}</li>"
+    html_content += "</ul>"
+    
+    return html_content
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
