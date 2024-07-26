91825b684af6803681c9f17b7d935df022402857
ashu sharma <ashusharma10071999@gmail.com>
2024-07-26 12:55:04 -0400

    remove def main

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-<<<<<<< HEAD
 
     students = [
         {"id": "c0894214", "name": "ashu sharma"},
@@ -20,10 +19,5 @@ def helloworld():
     
     return html_content
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
-=======
-    return "Hello From Azure!"
-if __name__ == "__main__":
-    app.run()
->>>>>>> da8c3580679eef7e5f90e6be574aa6d37423cc42
+app.run(host='0.0.0.0', port=80)
+
